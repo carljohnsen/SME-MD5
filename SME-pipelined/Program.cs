828c1db1d9dfa995433dd9488e113fc815fa7fe1
Carl Johnsen <cjjohnsen@nbi.ku.dk>
2017-10-23 11:11:16 +0200

    Added pipelined SME version

---
+++ SME-pipelined/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SME;
+
+namespace MD5Pipelined
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Simulation()
+                .BuildCSVFile()
+                .BuildVHDL()
+                .Run(typeof(Program).Assembly);
+        }
+    }
+}
