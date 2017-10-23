15ef940effb8e7d90772c73411c71547ab9b47cd
Carl Johnsen <cjjohnsen@nbi.ku.dk>
2017-10-23 11:07:32 +0200

    Added simple SME version

---
+++ SME-simple/Program.cs
@@ -0,0 +1,15 @@
+﻿using SME;
+
+namespace MD5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Simulation()
+                .BuildCSVFile()
+                .BuildVHDL()
+                .Run(typeof(Program).Assembly);
+        }
+    }
+}
---
+++ SME-simple/README.md
@@ -0,0 +1,3 @@
+These files depend on SME, which is found at
+
+https://github.com/kenkendk/sme
